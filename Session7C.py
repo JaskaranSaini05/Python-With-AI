e5ee42221a94b0026955e9470f5792300d4ca680
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-09-07 19:32:27 +0530

    Functions

---
+++ Session7C.py
@@ -0,0 +1,17 @@
+# Functions in Memory
+
+def add(num1,num2):
+    sum = num1 + num2
+    print('sum is:',sum)
+
+def multiply(num1,num2):
+    multiplication = num1 * num2 
+    print('multiplication is:',multiplication)
+
+number1 = 10
+number2 = 20
+
+add(number1,number2)
+multiply(number1,number2)
+
+print('program finished')
